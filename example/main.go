ac8f01c3549d437a5e67c97c94d0d7dbbf19aefa
xiaqb <xiaqiubo123@163.com>
2026-08-18 20:59:57 +0800

    feat: counter example

---
+++ example/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/opentoys/webview"
+)
+
+func main() {
+	w, err := webview.New(webview.Options{Debug: true})
+	if err != nil {
+		panic(err)
+	}
+	defer w.Close()
+
+	count := 0
+	if err := w.Bind("increment", func() int {
+		count++
+		return count
+	}); err != nil {
+		panic(err)
+	}
+
+	w.SetTitle("purego webview counter")
+	w.SetSize(600, 400, webview.SizeNone)
+	w.SetHTML(`<!doctype html>
+<html>
+<body style="font-family:system-ui;text-align:center;padding-top:2em">
+	<h1>PureGo WebView</h1>
+	<p id="c" style="font-size:2em">0</p>
+	<button onclick="increment().then(n => document.getElementById('c').textContent = n)"
+		style="font-size:1.2em;padding:0.5em 1em">+1</button>
+</body>
+</html>`)
+	if err := w.Run(); err != nil {
+		fmt.Println("run:", err)
+	}
+}
